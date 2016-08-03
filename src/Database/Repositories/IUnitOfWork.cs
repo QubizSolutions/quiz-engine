fc1e7c7595e36c518138a7b184d1bac1878529ab
Titus.Bocioc <titus.bocioc@gmail.com>
2016-08-03 10:20:22 +0300

    IUnitOfWork

--- src/Database/Repositories/IUnitOfWork.cs
+++ src/Database/Repositories/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace Qubiz.QuizEngine.Database.Repositories
     public interface IUnitOfWork
     {
         IFeatureFlagRepository FeatureFlagRepository { get; }
+        IAdminRepository AdminRepo { get; }
         Task SaveAsync();
     }
 }
