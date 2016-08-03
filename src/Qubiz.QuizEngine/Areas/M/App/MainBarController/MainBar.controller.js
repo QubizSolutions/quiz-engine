1bf230d3ad3ccf47da6a905b3c20d9b2eceac55d
Marian.Serban <Marian.Serban@ST32>
2016-08-03 14:36:41 +0300

    Fix(MenuBar) : Code cleaned

--- src/Qubiz.QuizEngine/Areas/M/App/MainBarController/MainBar.controller.js
+++ src/Qubiz.QuizEngine/Areas/M/App/MainBarController/MainBar.controller.js
@@ -1,22 +1,23 @@
 ﻿(function () {
-	'use strict'
-	angular.module('quizEngineMaterial').controller('MainBarTabsController', function ($location, $scope) {
+    'use strict'
+    angular.module('quizEngineMaterial').controller('MainBarTabsController', MainBarTabsController)
+    MainBarTabsController.$inject = ['$location','$scope'];
+    function MainBarTabsController (location, scope) {
+        scope.testsTabClick = function () {
+            location.path('/tests');
+        }
+        scope.examsTabClick = function () {
+            location.path('/exams');
+        }
+        scope.questionsTabClick = function () {
+            location.path('/questions');
 
-	    $scope.testsTabClick = function () {
-	        $location.path('/tests');
-	    }
-	    $scope.examsTabClick = function () {
-	        $location.path('/exams');
-	    }
-	    $scope.questionsTabClick = function () {
-	        $location.path('/questions');
-
-	    }
-	    $scope.sectionsTabClick = function () {
-	        $location.path('/sections');
-	    }
-	    $scope.administratorsTabClick = function () {
-	        $location.path('/administrators');
-	    }
-	});
+        }
+        scope.sectionsTabClick = function () {
+            location.path('/sections');
+        }
+        scope.administratorsTabClick = function () {
+            location.path('/administrators');
+        }
+    }
 })();
