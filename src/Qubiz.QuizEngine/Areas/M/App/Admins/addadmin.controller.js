a72937312bb347e3027cecffd8a93ac26e52eab9
Titus.Bocioc <titus.bocioc@gmail.com>
2016-08-04 13:15:18 +0300

    Feat(InputBar + AddAdmin template)

---
+++ src/Qubiz.QuizEngine/Areas/M/App/Admins/addadmin.controller.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+   angular
+      .module('quizEngineMaterial')
+      .controller('AddAdminController', AddAdminController)
+   AddAdminController.$inject = ['adminsService'];
+   function AddAdminController(adminsService)
+   {
+       var vm = this;
+       vm.Movie = {};
+       vm.Save = Save;
+       vm.Reset = Reset;
+       function Reset()
+       {
+           vm.Movie = {};
+       }
+       function Save()
+       {
+           //Need Admin Service Add
+           console.log("Saving works in controller");
+       }
+   }
+
+})();
