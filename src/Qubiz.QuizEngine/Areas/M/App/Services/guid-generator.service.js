eecd2538c123238479dc746c32f9baad9d2d5e13
Cristaling <ucanbeatit@gmail.com>
2016-08-08 18:47:23 +0300

    feat(guid-generator): added a way to generate guids and use them on the frontend

---
+++ src/Qubiz.QuizEngine/Areas/M/App/Services/guid-generator.service.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict'
+
+    angular
+        .module('quizEngineMaterial')
+        .service("guidGenerator", guidGenerator);
+
+    guidGenerator.$inject = ['$http'];
+
+    function guidGenerator($http) {
+        
+        var unusedGuids = [];
+        this.getNewGuid = getNewGuid;
+
+        generateNewGuids(10);
+
+        function generateNewGuids(number) {
+            return $http({
+                method: 'GET',
+                url: 'api/GuidGenerator/GetGuids/',
+                params: {
+                    number: number
+                }
+            });
+        }
+
+        function getNewGuid() {
+            var result = unusedGuids.pop();
+            if (unusedGuids.length == 0) {
+                generateNewGuids();
+            }
+            return result;
+        }
+    }
+})();
