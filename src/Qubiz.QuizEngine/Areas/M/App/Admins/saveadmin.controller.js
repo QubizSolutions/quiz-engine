deb9075c6abda6ee1e8eb083f30c027066bdaa0a
Titus.Bocioc <titus.bocioc@gmail.com>
2016-08-09 15:53:57 +0300

    Feat(SaveAdmin new template)

--- src/Qubiz.QuizEngine/Areas/M/App/Admins/saveadmin.controller.js
+++ src/Qubiz.QuizEngine/Areas/M/App/Admins/saveadmin.controller.js
@@ -10,43 +10,66 @@
     function SaveAdminController(adminsService, location, mdDialog,$routeParams) {
 
         var vm = this;
-        adminsService.GetById($routeParams.id).then(function(result)
-        {
+        var originalAdmin;
+        adminsService.GetById($routeParams.id)
+        .then(function (result) {
             vm.Admin = result.data;
-            if (vm.Admin == null) {
+            if (vm.Admin != null) {
+                originalAdmin = vm.Admin.Name;
+                vm.Reset = function () {
+                    vm.Admin.Name = originalAdmin;
+                }
+                vm.Save = editAdmin;
+            }
+            else {
                 vm.Save = addAdmin;
                 vm.Admin = {};
                 vm.Admin.ID = $routeParams.id;
+                vm.Reset = reset;
+                console.log(vm.Admin);
+
             }
-            else
-                vm.Save = editAdmin;
-        })
-        vm.Reset = reset;
+        }
+        );
+        
+    
+
 
         function reset() {
             vm.Admin = {};
         }
 
          
-        function addAdmin()
-        {
-          adminsService.AddAdmin(vm.Admin).then(function () {
-                        location.path('/administrators');
-               })
-                }
+        function addAdmin() {
+
+            console.log(vm.Admin);
+            if (vm.Admin.Name == "")
+            {
+                mdDialog.show(mdDialog
+                    .alert()
+                    .title('Error')
+                    .textContent('Admin name cannot be empty!')
+                    .ok('Ok!'));
+            }
+            adminsService.AddAdmin(vm.Admin)
+                .then(function () {
+                            location.path('/administrators');
+                })
+                .catch(function () {
+                   mdDialog.show(mdDialog
+                  .alert()
+                  .title('Error')
+                  .textContent('This admin already exists.')
+                  .ok('Ok!'));
+                });
+         }
         function editAdmin()
         {
             adminsService.EditAdmin(vm.Admin)
                 .then(function () {
-                        location.path('/administrators');
-                 })
-                .catch(function () {
-                    mdDialog.show(mdDialog
-                        .alert()
-                        .title('Error')
-                        .textContent('This admin already exists.')
-                        .ok('Ok!'));
-                 });
+                    location.path('/administrators');
+                });
+
           }
 
      }
