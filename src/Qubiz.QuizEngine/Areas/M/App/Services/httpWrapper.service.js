4dbfb0fcb1bb3f82e958da2f2517b9ce6c27ca20
DarkYonnu <DarkYonnu@GAMESTASTION>
2016-08-29 20:49:52 +0300

    Feat (serviceWrapper) : implemented service wrapper for http functions

---
+++ src/Qubiz.QuizEngine/Areas/M/App/Services/httpWrapper.service.js
@@ -0,0 +1,61 @@
+﻿(function () {
+    'use strict'
+
+    angular
+        .module('quizEngineMaterial')
+        .service("httpWrapperService", httpWrapperService);
+
+    httpWrapperService.$inject = ['$http', '$q'];
+
+    function httpWrapperService($http, $q) {
+
+        this.get = get;
+        this.post = post;
+        this.put = put;
+        this.delete = del;
+
+        function get(getUrl, data) {
+            var defer = $q.defer();
+            $http.get(getUrl)
+                .then(function (response) {
+                    defer.resolve(response.data);
+                }, function (response) {
+                    defer.reject(response);
+                });
+            return defer.promise;
+        }
+
+        function post(postUrl, data) {
+            var defer = $q.defer();
+            $http.post(postUrl + data.ID)
+                 .then(function (response) {
+                     defer.resolve(response.data);
+                 }, function (response) {
+                     defer.reject(response);
+                 });
+            return defer.promise;
+        }
+
+        function put(putUrl, data) {
+            var defer = $q.defer();
+            $http.put(putUrl + data.ID)
+                .then(function (response) {
+                    defer.resolve(response.data);
+                }, function (response) {
+                    defer.reject(response);
+                });
+            return defer.promise;
+        }
+
+        function del(deleteUrl, data) {
+            var defer = $q.defer();
+            $http.delete(deleteUrl + data.ID)
+              .then(function (response) {
+                  defer.resolve(response.data);
+              }, function (response) {
+                  defer.reject(response);
+              });
+            return defer.promise;
+        }
+    }
+})();
