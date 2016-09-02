c51a03fa7f13d65c5776d91bb34efff3359af913
MarianCristian <maryan109@yahoo.ro>
2016-09-02 14:54:55 +0300

    fix(SectionRepositoryTest) : Made methods to return an async task

--- src/UnitTesting/Database/SectionRepositoryTest.cs
+++ src/UnitTesting/Database/SectionRepositoryTest.cs
@@ -5,6 +5,7 @@ using Qubiz.QuizEngine.Database;
 using Qubiz.QuizEngine.Database.Repositories;
 using Qubiz.QuizEngine.Database.Entities;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Qubiz.QuizEngine.UnitTesting.Database
 {
@@ -29,7 +30,7 @@ namespace Qubiz.QuizEngine.UnitTesting.Database
         }
 
         [TestMethod]
-        public void ListAsync_ExistingSections_ReturnArrayOfSections()
+        public async Task ListAsync_ExistingSections_ReturnArrayOfSections()
         {
             Section section1 = new Section { ID = Guid.NewGuid(), Name = "Section1" };
             Section section2 = new Section { ID = Guid.NewGuid(), Name = "Section2" };
@@ -37,65 +38,66 @@ namespace Qubiz.QuizEngine.UnitTesting.Database
             sectionRepository.Upsert(section1);
             sectionRepository.Upsert(section2);
 
-            Section[] dbSections = sectionRepository.ListAsync().Result;
+            Section[] dbSections = await sectionRepository.ListAsync();
+
             AssertSectionEqual(section1, dbSections.First(section => section.ID == section1.ID));
             AssertSectionEqual(section1, dbSections.First(section => section.ID == section1.ID));
         }
 
         [TestMethod]
-        public void GetByIDAsync_ExistingSection_ReturnsSectionByID()
+        public async Task GetByIDAsync_ExistingSection_ReturnsSectionByID()
         {
             Section section = new Section { ID = Guid.NewGuid(), Name = "TestSection" };
 
             sectionRepository.Upsert(section);
 
-            Section dbSection = sectionRepository.GetByIDAsync(section.ID).Result;
+            Section dbSection = await sectionRepository.GetByIDAsync(section.ID);
 
             AssertSectionEqual(section, dbSection);
         }
 
         [TestMethod]
-        public void GetByNameAsync_ExistingSection_ReturnsSectionByName()
+        public async Task GetByNameAsync_ExistingSection_ReturnsSectionByName()
         {
             Section section = new Section { ID = Guid.NewGuid(), Name = "TestSection" };
 
             sectionRepository.Upsert(section);
 
-            Section dbSection = sectionRepository.GetByNameAsync(section.Name).Result;
+            Section dbSection = await sectionRepository.GetByNameAsync(section.Name);
 
             AssertSectionEqual(section, dbSection);
+        }
+
+        [TestMethod]
+        public async Task ListAsync_UnexistingSections_ReturnEmptyList()
+        {
+            Section[] sections = await sectionRepository.ListAsync();
+
+            Assert.IsTrue(sections.Count() == 0);
+        }
+
+        [TestMethod]
+        public async Task GetByIDAsync_UnexistingSection_ReturnsNull()
+        {
+            Guid id = Guid.NewGuid();
+
+            Section section = await sectionRepository.GetByIDAsync(id);
+
+            Assert.IsNull(section);
+        }
+
+        [TestMethod]
+        public async Task GetByNameAsync_UnexistingSection_ReturnsNull()
+        {
+            Section section = await sectionRepository.GetByNameAsync("UnexistingSection");
+
+            Assert.IsNull(section);
         }
 
         private void AssertSectionEqual(Section expected, Section actual)
         {
             Assert.AreEqual(expected.ID, actual.ID);
             Assert.AreEqual(expected.Name, actual.Name);
-        }
-
-        [TestMethod]
-        public void ListAsync_UnexistingSections_ReturnEmptyList()
-        {
-            Section[] sections = sectionRepository.ListAsync().Result;
-
-            Assert.IsTrue(sections.Count() == 0);
-        }
-
-        [TestMethod]
-        public void GetByIDAsync_UnexistingSection_ReturnsNull()
-        {
-            Guid id = Guid.NewGuid();
-
-            Section section = sectionRepository.GetByIDAsync(id).Result;
-
-            Assert.IsNull(section);
-        }
-
-        [TestMethod]
-        public void GetByNameAsync_UnexistingSection_ReturnsNull()
-        {
-            Section section = sectionRepository.GetByNameAsync("UnexistingSection").Result;
-
-            Assert.IsNull(section);
         }
     }
 }
