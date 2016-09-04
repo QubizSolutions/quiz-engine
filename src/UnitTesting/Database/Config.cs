8a1841758eb82b5144ffba24644ac80f2c4458e7
Marian Cristian <Marian Cristian@DESKTOP-7KF42UJ>
2016-09-04 22:25:36 +0300

    fix(config) : changed order for connection strings load

--- src/UnitTesting/Database/Config.cs
+++ src/UnitTesting/Database/Config.cs
@@ -14,17 +14,19 @@ namespace Qubiz.QuizEngine.UnitTesting.Database
         {
             get
             {
-                _ConnectionString = ConfigurationManager.ConnectionStrings["qubizQuizEngineTest"].ConnectionString;
-                if (_ConnectionString == null) {
-                    string localConfigFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("file:\\", ""), @"..\..\..\")) + "config.local";
 
-                    if (File.Exists(localConfigFilePath))
-                    {
-                        XDocument moduleConfig = XDocument.Load(localConfigFilePath);
+                string localConfigFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("file:\\", ""), @"..\..\..\")) + "config.local";
 
-                        _ConnectionString = moduleConfig.XPathSelectElements("/configuration/test").Select(x => x.Attribute("connectionString").Value).First();
-                    }
+                if (File.Exists(localConfigFilePath))
+                {
+                    XDocument moduleConfig = XDocument.Load(localConfigFilePath);
+
+                    _ConnectionString = moduleConfig.XPathSelectElements("/configuration/test").Select(x => x.Attribute("connectionString").Value).First();
+                }else
+                {
+                    _ConnectionString = ConfigurationManager.ConnectionStrings["qubizQuizEngineTest"].ConnectionString;
                 }
+
                 return _ConnectionString;
             }
         }
