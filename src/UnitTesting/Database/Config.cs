e64819e7673dab26ff7bfd2074bc3bcc812f7916
MarianCristian <maryan109@yahoo.ro>
2016-09-05 10:37:48 +0300

    code clean up and git ignore config.local

--- src/UnitTesting/Database/Config.cs
+++ src/UnitTesting/Database/Config.cs
@@ -9,25 +9,20 @@ namespace Qubiz.QuizEngine.UnitTesting.Database
 {
     public class Config : IConfig
     {
-        private string _ConnectionString;
         public string ConnectionString
         {
             get
             {
-
                 string localConfigFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("file:\\", ""), @"..\..\..\")) + "config.local";
 
                 if (File.Exists(localConfigFilePath))
                 {
                     XDocument moduleConfig = XDocument.Load(localConfigFilePath);
 
-                    _ConnectionString = moduleConfig.XPathSelectElements("/configuration/test").Select(x => x.Attribute("connectionString").Value).First();
-                }else
-                {
-                    _ConnectionString = ConfigurationManager.ConnectionStrings["qubizQuizEngineTest"].ConnectionString;
+                    return moduleConfig.XPathSelectElements("/configuration/test").Select(x => x.Attribute("connectionString").Value).First();
                 }
 
-                return _ConnectionString;
+                return ConfigurationManager.ConnectionStrings["qubizQuizEngineTest"].ConnectionString;
             }
         }
     }
