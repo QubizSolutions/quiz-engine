b8eb2dd0b6dc1604cdafe588a5c5c2bdd4834fcf
Marian Cristian <marian.serban@qubiz.com>
2016-09-14 17:49:52 +0300

    refactor(section-repository): T315 - Move files to their proper namespace (#37)
    refactor(section-repository): T315 - Move files to their proper namespace

--- src/Database/Repositories/UnitOfWork.cs
+++ src/Database/Repositories/UnitOfWork.cs
@@ -1,100 +1,102 @@
-﻿using Qubiz.QuizEngine.Infrastructure;
+﻿using Qubiz.QuizEngine.Database.Repositories.Section;
+using Qubiz.QuizEngine.Database.Repositories.Section.Contract;
+using Qubiz.QuizEngine.Infrastructure;
 using System;
 using System.Threading.Tasks;
 
 namespace Qubiz.QuizEngine.Database.Repositories
 {
-    public class UnitOfWork : IUnitOfWork, IDisposable
-    {
-        private readonly QuizEngineDataContext dbContext;
-        
-        private IFeatureFlagRepository featureFlagRepository;
-        private IQuestionRepository questionRepository;
-        private IOptionRepository optionRepository;
-        private ISectionRepository sectionRepository;
-        private IAdminRepository adminsRepository;
+	public class UnitOfWork : IUnitOfWork, IDisposable
+	{
+		private readonly QuizEngineDataContext dbContext;
 
-        public UnitOfWork(IConfig config)
-        {
-            dbContext = new QuizEngineDataContext(config.ConnectionString);
-        }
+		private IFeatureFlagRepository featureFlagRepository;
+		private IQuestionRepository questionRepository;
+		private IOptionRepository optionRepository;
+		private ISectionRepository sectionRepository;
+		private IAdminRepository adminsRepository;
 
-        public IFeatureFlagRepository FeatureFlagRepository
-        {
-            get
-            {
-                if (featureFlagRepository == null)
-                {
-                    featureFlagRepository = new FeatureFlagRepository(dbContext, this);
-                }
+		public UnitOfWork(IConfig config)
+		{
+			dbContext = new QuizEngineDataContext(config.ConnectionString);
+		}
 
-                return featureFlagRepository;
-            }
-        }
+		public IFeatureFlagRepository FeatureFlagRepository
+		{
+			get
+			{
+				if (featureFlagRepository == null)
+				{
+					featureFlagRepository = new FeatureFlagRepository(dbContext, this);
+				}
 
-        public IQuestionRepository QuestionRepository
-        {
-            get
-            {
-                if (questionRepository == null)
-                {
-                    questionRepository = new QuestionRepository(dbContext, this);
-                }
+				return featureFlagRepository;
+			}
+		}
 
-                return questionRepository;
-            }
-        }
+		public IQuestionRepository QuestionRepository
+		{
+			get
+			{
+				if (questionRepository == null)
+				{
+					questionRepository = new QuestionRepository(dbContext, this);
+				}
 
-        public IOptionRepository OptionRepository
-        {
-            get
-            {
-                if (optionRepository == null)
-                {
-                    optionRepository = new OptionRepository(dbContext, this);
-                }
+				return questionRepository;
+			}
+		}
 
-                return optionRepository;
-            }
-        }
+		public IOptionRepository OptionRepository
+		{
+			get
+			{
+				if (optionRepository == null)
+				{
+					optionRepository = new OptionRepository(dbContext, this);
+				}
 
-        public ISectionRepository SectionRepository
-        {
-            get
-                {
-                if (sectionRepository == null)
-                {
-                    sectionRepository = new SectionRepository(dbContext);
-                }
+				return optionRepository;
+			}
+		}
 
-                return sectionRepository;
-            }
-        }
+		public ISectionRepository SectionRepository
+		{
+			get
+			{
+				if (sectionRepository == null)
+				{
+					sectionRepository = new SectionRepository(dbContext);
+				}
 
-        public IAdminRepository AdminRepository
-        {
-            get
-            {
-                if (adminsRepository == null)
-                {
-                    adminsRepository = new AdminRepository(dbContext, this);
-                }
+				return sectionRepository;
+			}
+		}
 
-                return adminsRepository;
-            }
-        }
+		public IAdminRepository AdminRepository
+		{
+			get
+			{
+				if (adminsRepository == null)
+				{
+					adminsRepository = new AdminRepository(dbContext, this);
+				}
 
-        public async Task SaveAsync()
-        {
-            await dbContext.SaveChangesAsync();
-        }
+				return adminsRepository;
+			}
+		}
 
-        public void Dispose()
-        {
-            if (dbContext != null)
-            {
-                dbContext.Dispose();
-            }
-        }
-    }
+		public async Task SaveAsync()
+		{
+			await dbContext.SaveChangesAsync();
+		}
+
+		public void Dispose()
+		{
+			if (dbContext != null)
+			{
+				dbContext.Dispose();
+			}
+		}
+	}
 }
